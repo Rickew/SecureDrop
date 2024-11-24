fc870dee5e710bd263dce11816bd1374654fb668
Rickew <pewhite424@gmail.com>
2024-11-24 12:07:18 -0500

    Push on Sun 11/24/2024 From: DESKTOP-261RCK5

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 # requires pyinstaller
 #linux
-pyinstaller ./Securedrop.py --distpath ./releases/linux --name secure_drop --onefile 
+mkdir linuxdistro
+cp ./Securedrop.py ./secure_drop.py
+pyinstaller ./secure_drop.py --distpath ./releases/linux --onefile
 cp ./releases/linux/secure_drop ./
+rm secure_drop.py
