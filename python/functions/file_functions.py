fd6af4f991ca0ea3395137efcefdd7532e5a5016
Rickew <pewhite424@gmail.com>
2024-11-25 14:45:08 -0500

    Push on Mon 11/25/2024 From: DESKTOP-GGACHD

--- python/functions/file_functions.py
+++ python/functions/file_functions.py
@@ -8,7 +8,7 @@ def get_file() -> str:
     var = getattr(sys, "frozen", False), system()
     if var[0]:
         if var[1] == "Linux":
-            filedir = "~/.scdusers/usersfile.json" # linux path if it is installed
+            filedir = "home/$USER/.scdusers/usersfile.json" # linux path if it is installed
         else:
             filedir = os.path.dirname(sys.executable) # gets the directory of the program, if the program if an exectutable
             filedir += "\\scdusers\\usersfile.json" # windows path
