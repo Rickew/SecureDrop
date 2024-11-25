1a12aee480e24c17876959b7a073af4bf782b237
Rickew <pewhite424@gmail.com>
2024-11-25 15:20:08 -0500

    Push on Mon 11/25/2024 From: DESKTOP-GGACHD

--- releases/linux/install.sh
+++ releases/linux/install.sh
@@ -1,4 +1,4 @@
 cp ./secure_drop /usr/bin/
 chmod +x /usr/bin/secure_drop
 cp -r ./scdusers /home/$SUDO_USER/.scdusers
-chwon $SUDO_USER:$(id -gn) /home/$SUDO_USER/.scdusers
+chown $SUDO_USER:$(id -gn) /home/$SUDO_USER/.scdusers
